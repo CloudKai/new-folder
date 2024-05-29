d461e2b2a37834ec1a0ca5cbe55e235c231d217c
Cloudkai <kaiming_01@live.com>
2024-05-30 05:00:21 +0800

    new features

---
+++ DailyEmote/styleSheets/Styles.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  
+  container: {
+    flex: 1,
+    backgroundColor: '#0C0C1E',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 0,
+    
+  },
+
+  textInput: {
+    marginVertical: 4,
+    height: 50,
+    width: 300,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+});
